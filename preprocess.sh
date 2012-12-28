5d9b8a3405a760fdb897b6bcb79158910bf76d5e
Nathan Wilcox <nejucomo@gmail.com>
2012-12-27 20:41:18 -0800

    Add preprocess.sh as an attempt to debug shroud.

---
+++ preprocess.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ex
+
+tmp="$(mktemp -d nejucomos_dungeon_crawl_preprocessed.XXXXX)"
+
+bootstrap="$tmp/bootstrap.cfg"
+output="$tmp/output"
+
+ln -s "$(readlink -f './nejucomos_dungeon_crawl/')" "$tmp/"
+
+cat > "$bootstrap" <<EOF
+## CORE UNITS ##
+{core/units.cfg}
+
+## nejucomos_dungeon_crawl ##
+{VARIABLE NDC_PREPROCESS_SEPARATOR (===)}
+{./nejucomos_dungeon_crawl}
+EOF
+
+wesnoth --preprocess "$bootstrap" "$output" --preprocess-defines MULTIPLAYER
+
+cat "$output/bootstrap.cfg"
+
+rm -r "$tmp"
+
+
